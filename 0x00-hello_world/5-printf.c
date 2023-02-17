11ecaea1c1d4edf335c38b108ad0b1f9e0eb3cec
Otavie <otavieokuoyo@gmail.com>
2023-02-16 17:33:53 -0800

    Added the 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - entry point of the program
- * return - 0 for success
+ * return: 0 for success
  * */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }
