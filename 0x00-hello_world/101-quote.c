9e23214b7ade92c674c6428cd70050d176c8a094
Otavie <otavieokuoyo@gmail.com>
2023-02-16 19:20:04 -0800

    Added the 101-quote file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,9 @@
  *
  */
 int main(void)
-{
-	int ln = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", ln);
+{	
+	int le = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", le);
 	return (1);
 }
