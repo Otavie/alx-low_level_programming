2cd3c6917ecde15b6dc94157253db45e9efa2c0d
Otavie <otavieokuoyo@gmail.com>
2023-02-16 18:41:33 -0800

    Added the 101-quote file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - entry point of the program
+ * Return: 1 (error)
+ *
+ */
+int main(void)
+{
+	int len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
+	return (1);
+}
