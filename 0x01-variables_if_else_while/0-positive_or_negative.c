81579ce7631577627118f9d13b1daaa689e67af3
Otavie <otavieokuoyo@gmail.com>
2023-02-17 14:55:37 -0800

    Added 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,10 +19,12 @@ int main(void)
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
-	}else if (n < 0)
+	}
+	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
-	} else
+	}
+	else
 	{
 		printf("%d is zero\n", n);
 	}
