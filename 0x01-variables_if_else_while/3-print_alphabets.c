78ccea966a3983531c489bb6d68c7273ebbcdd36
Otavie <otavieokuoyo@gmail.com>
2023-02-17 16:48:02 -0800

    Added the 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,10 +2,20 @@
 /**
 * main - entry point of the program
 *
-* Return 0: indicates success
+* Return: Always 0 (Success)
 **/
 int main(void)
 {
-return (0);
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	
+	putchar('\n');
+
+	return (0);
 }
 
