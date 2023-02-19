19853e59cb1c4e5959d175180b01493ab0c26262
Otavie <otavieokuoyo@gmail.com>
2023-02-19 13:37:50 -0800

    Added 102-print_combo5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,11 +13,11 @@ int main(void)
 	{
 		for (j = i + 1; j <= 99; j++)
 		{
-			putchar(i/10 + '0');
-			putchar(i%10 + '0');
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
 			putchar(' ');
-			putchar(j/10 + '0');
-			putchar(j%10 + '0');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
 
 			if (i != 98 || j != 99)
 			{
