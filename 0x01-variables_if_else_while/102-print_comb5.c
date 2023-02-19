a47ae0fee73b851e0d65e650a0fb215c0a38df8d
Otavie <otavieokuoyo@gmail.com>
2023-02-19 12:57:49 -0800

    Added 102-print_combo5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/*
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+*/
+int main (void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(i + '0');
+			putchar(' ');
+			putchar(i + '0');
+			putchar(j + '0');
+
+			if (i != 9 && j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
