cdca2172269340ade91a95c1946ad04d561f31f9
Otavie <otavieokuoyo@gmail.com>
2023-02-21 12:37:55 -0800

    Added 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,18 @@
-/*\n* main - entry point of the program\n*\n* Return: Always 0 (Success)\n*/\n\nint main(void)\n{\n\nreturn (0);\n}
+#include "main.h"
+
+/**
+ * _isalpha - a program that checks for alphabet
+ * @c: The character to check.
+ * Return: 1 if the character is lowercase or uppercase, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
