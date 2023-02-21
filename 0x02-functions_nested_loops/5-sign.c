25f7c355c400abba8c0c939a9c167ea69f549133
Otavie <otavieokuoyo@gmail.com>
2023-02-21 13:00:44 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,22 @@
-/*\n* main - entry point of the program\n*\n* Return: Always 0 (Success)\n*/\n\nint main(void)\n{\n\nreturn (0);\n}
+#include "main.h"
+
+/**
+ * sign_print - a program that prints the sign of a number.
+ * @n: The number to check.
+ * Return: 1 if the number is +, -1 if number is - and 0 otherwise.
+ */
+int sign_print(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
