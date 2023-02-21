ebf729d8b14fe2ea1a5e80b8f37fbd228d18c8cf
Otavie <otavieokuoyo@gmail.com>
2023-02-21 14:01:44 -0800

    Added the 5-sign.c file

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -10,7 +10,7 @@ int main(void)
 	int r;
 
 	r = print_sign(98);
-	_putchar(';');
+	_putchar(',');
 	_putchar(' ');
 	_putchar(r + '0');
 	_putchar('\n');
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,25 @@
 #include "main.h"
 
 /**
- * sign_print - a program that prints the sign of a number.
+ * print_sign - a program that prints the sign of a number.
  * @n: The number to check.
  * Return: 1 if the number is +, -1 if number is - and 0 otherwise.
  */
-int sign_print(int n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar('+');
 		return (1);
 	}
 	else if (n < 0)
 	{
+		_putchar('-');
 		return (-1);
 	}
 	else
 	{
+		_putchar('0');
 		return (0);
 	}
 }
