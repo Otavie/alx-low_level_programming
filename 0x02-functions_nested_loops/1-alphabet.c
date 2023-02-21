05c65ccd72ad595204f77a3d229ddfa36eaf0c70
Otavie <otavieokuoyo@gmail.com>
2023-02-21 10:39:41 -0800

    Added the 1-alphabet.c file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /*
- * print_alphabet - prints all the alphabets in lowercase, followed by a new line
+ * print_alphabet - prints the alphabets in lowercase, followed by a new line
 */
 void print_alphabet(void)
 {
