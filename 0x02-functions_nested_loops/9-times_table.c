62b8a638119d8566deb35071704400d747362902
Otavie <otavieokuoyo@gmail.com>
2023-02-21 20:18:35 -0800

    Added the 9-times_table.c file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,27 @@
-/*\n* main - entry point of the program\n*\n* Return: Always 0 (Success)\n*/\n\nint main(void)\n{\n\nreturn (0);\n}
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - a function that prints every minute of the day.
+ * Return: Always 0 (Success).
+ */
+void times_table(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			if (y == 0)
+			{
+				printf("%d", x * y);
+			}
+			else
+			{
+				printf(", %d", x * y);
+			}
+		}
+		printf("\n");
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,5 +12,6 @@ int print_last_digit(int);
 int add(int, int);
 void print_to_98(int n);
 void jack_bauer(void);
+void times_table(void);
 
 #endif
