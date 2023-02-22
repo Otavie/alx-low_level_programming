0b13d036e6ec5cc4a98e2b1e88606edc2f76a6ee
Otavie <otavieokuoyo@gmail.com>
2023-02-21 20:00:20 -0800

    Added 8-24_hours.c file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,19 @@
-/*\n* main - entry point of the program\n*\n* Return: Always 0 (Success)\n*/\n\nint main(void)\n{\n\nreturn (0);\n}
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - a function that prints every minute of the day.
+ * Return: Always 0 (Success).
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			printf("%02d:%02d\n", h, m);
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,5 +11,6 @@ int _abs(int);
 int print_last_digit(int);
 int add(int, int);
 void print_to_98(int n);
+void jack_bauer(void);
 
 #endif
