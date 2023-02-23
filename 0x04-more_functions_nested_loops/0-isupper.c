8278018f40abe72cfcf8659d1b45d73ef55a7545
Otavie <otavieokuoyo@gmail.com>
2023-02-23 10:52:27 -0800

    Added the 0-isupper.c file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,12 @@
 
 /**
  * _isupper - a program that checks for uppercase character
- *
  * @c: character to be checked
- *
  * Return: 1 if the character is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <='Z')
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
