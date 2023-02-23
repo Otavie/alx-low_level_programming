6beb283509a0b01c80028ef2301a8afce3e9134b
Otavie <otavieokuoyo@gmail.com>
2023-02-23 12:57:59 -0800

    Added the 6-print_line.c file

---
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_line(0);
+	print_line(1);
+	print_line(10);
+	print_line(-4);
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - a function that draws a straight line
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+}
