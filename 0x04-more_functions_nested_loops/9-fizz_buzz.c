f787cd57e211ee104c765ae3d98de5d25652e476
Otavie <otavieokuoyo@gmail.com>
2023-02-23 14:28:44 -0800

    Added the file 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+void print_fizz_buzz(void);
+
+int main(void)
+{
+	print_fizz_buzz();
+
+	return (0);
+}
+
+/**
+* print_fizz_buzz - a program that prints fizz, buzz based on criteria
+*/
+
+void print_fizz_buzz(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 15) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((n % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((n % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			if (n == 100)
+			{
+				printf("%d\n", n);
+			}
+			else
+			{
+				printf("%d ", n);
+			}
+		}
+	}
+}
