66e4333d7c52664bdb64e1ea9261fde1b847c4ab
Otavie <otavieokuoyo@gmail.com>
2023-02-23 18:10:14 -0800

    Added the files 2-largest_number.c, main.h

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,4 +11,19 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
+
+	if ((a > b && b > c) || (a > c && c > b))
+	{
+		largest = a;
+	}
+	else if ((b > a && a > c) || (b > c && c > a))
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
 }
