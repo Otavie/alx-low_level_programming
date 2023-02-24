d1186f65a25833f1cebd0970e76ea612ed42b565
Otavie <otavieokuoyo@gmail.com>
2023-02-23 20:32:41 -0800

    Added the files 2-largest_number.c, main.h

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
- * largest_number - a program that prints the largest of 3 integers
+ * largest_number - a program that prints the largest
+ * of 3 integers
+ *
  * @a: first integer
  * @b: second integer
  * @c: third integer
+ *
  * Return: largest number
  */
 
