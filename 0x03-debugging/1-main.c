31fe468d2d892fe87a7356061da874e003d5480a
Otavie <otavieokuoyo@gmail.com>
2023-02-23 17:34:02 -0800

    Added the 1-main.c file

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,12 +11,12 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-	i = 0;
-/**	
- *	while (i < 10)*/
+/*	i = 0;
+	
+	while (i < 10)
 	{
 		putchar(i);
-	}
+	}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
