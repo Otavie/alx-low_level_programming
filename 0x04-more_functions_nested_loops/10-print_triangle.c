0e75a2a40d689dea96c3359983352978d8fd1ee9
Otavie <otavieokuoyo@gmail.com>
2023-02-23 16:21:51 -0800

    Added the 10-print_triangle.c file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_trianglee - a function that prints a triangle
+ * @size: the size of the square
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = size; i > 0; i--)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+}
