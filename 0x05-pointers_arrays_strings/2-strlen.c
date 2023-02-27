59d40fe16db16d31061ee310eab6ac3d4a0b72c3
Otavie <otavieokuoyo@gmail.com>
2023-02-27 13:06:52 -0800

    Added the 2-strlen.c files

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - a function that returns the legth of a string
+ *
+ * @s: The string to be checked
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
