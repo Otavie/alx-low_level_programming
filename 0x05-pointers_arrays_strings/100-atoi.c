1152f3fa125b9668f002294ea4c8732b12122ca8
Otavie <otavieokuoyo@gmail.com>
2023-02-27 19:18:56 -0800

    Added the 100-atoi.c file

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _atoi - function that converts a string to an integer
+ *
+ * @s: The string to be converted
+ */
+int _atoi(char *s)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
