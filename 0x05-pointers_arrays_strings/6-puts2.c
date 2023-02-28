a3b19fa64906e14e0fd4c986472688e0ad5cddf9
Otavie <otavieokuoyo@gmail.com>
2023-02-27 16:26:24 -0800

    Added the 6-puts.c file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * puts2 - function that prints every other cgaracter
+ *
+ * @str: String to be printed
+ */
+void puts2(char *str)
+{
+	int len = strlen(str);
+	int i;
+
+	for (i = 0; i < len - 1; i++)
+	{
+		if (i % 2 == 0)
+		{
+			printf("%d", i);
+		}
+	};
+	printf("\n");
+}
