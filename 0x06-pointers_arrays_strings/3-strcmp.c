97ca15a62bfa979c327a68c09cbec2c359f2c653
Otavie <otavieokuoyo@gmail.com>
2023-03-01 04:20:33 -0800

    Added the 4-rev_array.c file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_strcmp - function that compares two strings
+ *
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Return: Integer that indicates the result of the comparison
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_strcmp - function that compares two strings
+ *
+ * @a: An array of integers
+ * @n: The number of elements in the array
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		temp = a[i];
+		a[i] = a[n - i + 1];
+		a[n - i + 1] = temp;
+	}
+}
