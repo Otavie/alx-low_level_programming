7df4d53e25de3fc110dbed742c3218172c818524
Otavie <otavieokuoyo@gmail.com>
2023-03-02 16:27:16 -0800

    Wrote 1-strncat.c without std libraries

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,19 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 #include <string.h>
 
 /**
- * *_strncat - function that concatenates two strings
+ * *_strncat - function that concatenates two strings up to n bytes
  *
  * @src: The source string
  * @dest: The destinnation string
- * @n: Number of bytes to be used from src
+ * @n: The maximum number of bytes to copy from
  *
  * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
+	char *p = dest;
+	const char *q = src;
+
+	/* Find the end of the destination string */
+	while (*p != '\0')
+	{
+		p++;
+	}
+
+	/* Append up to n bytes from the source string */
+	while (*q != '\0' && n--)
+	{
+		*p++ = *q++;
+	}
+	*p = '\0';
 
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,7 +13,7 @@ void reverse_array(int *a, int n)
 {
 	int i, temp;
 
-	for (i = 0; i > n; i++)
+	for (i = 0; i > n / 2; i++)
 	{
 		temp = a[i];
 		a[i] = a[n - i + 1];
