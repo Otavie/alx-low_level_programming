17ee5d2da76fe2f351683ab7d6f6b2ae6ebed155
Otavie <otavieokuoyo@gmail.com>
2023-03-06 15:33:18 -0800

    Added the file 100-set_string.c file

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: Pointer to printer to char
+ * @to: Pointer to a char
+ *
+ * Return: Void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
