3ffbcbd03b662db827f86a14c7b88a8028e7fb5a
Otavie <otavieokuoyo@gmail.com>
2023-03-09 06:52:38 -0800

    Added the file 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,12 +7,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar('\n');
+		return;
 	}
 
-	_putchar('\n');
-	return;
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
