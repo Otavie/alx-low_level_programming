26d8782db00964b95be4b13acabdb23d065e5596
Otavie <otavieokuoyo@gmail.com>
2023-03-09 07:28:01 -0800

    Added the file 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,14 +7,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar(*s);
-		_print_rev_recursion(s - 1);
+		_print_rev_recursion(s + 1);
 	}
-	else
-	{
-		_putchar('\n');
-		return;
-	}
+	_putchar(*s);
 }
