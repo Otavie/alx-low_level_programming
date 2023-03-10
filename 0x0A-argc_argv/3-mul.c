d13669358b9118660c9e70a3f820f0851f8e2663
Otavie <otavieokuoyo@gmail.com>
2023-03-10 10:59:09 -0800

    Added the file 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the result of the mul of two numbers
+ *
+ * @argc: The number of command line arguments (not used)
+ * @argv: An array of command line argument strings
+ *
+ * Return: Always 0 (Success).
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+	int result;
+
+	if (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		result = a * b;
+	}
+	else
+	{
+		printf("%s\n", "Arguments not complete");
+	}
+
+	printf("%d\n", result);
+
+	return (0);
+}
