50bf9d25592a822e3df4b0d1f904f7880cd83762
Otavie <otavieokuoyo@gmail.com>
2023-03-10 12:47:31 -0800

    Added the 100-change.c file

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,19 +9,19 @@
  */
 int main(int argc, char *argv[])
 {
-	int cents, int coins = 0;
+	int cents, coins = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	cents = atoi(argv[1]);
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	cents = atoi(argv[1]);
 	while (cents > 0)
 	{
 		if (cents >= 20)
