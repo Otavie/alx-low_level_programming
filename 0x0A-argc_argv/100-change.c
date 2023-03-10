a8c97d936916474b56d0437c091675b247591629
Otavie <otavieokuoyo@gmail.com>
2023-03-10 12:38:08 -0800

    Added the 100-change.c file

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,10 +3,8 @@
 
 /**
  * main - prints the minimum number of coins to make a change
- *
  * @argc: The number of command line arguments (not used)
  * @argv: An array of command line argument strings
- *
  * Return: Always 0 (Success).
  */
 int main(int argc, char *argv[])
@@ -15,23 +13,17 @@ int main(int argc, char *argv[])
 	int coins = 0;
 
 	if (argc != 2)
-	{
 		printf("Error\n");
 		return (1);
-	}
 	cents = atoi(argv[1]);
 	if (cents < 0)
-	{
 		printf("0\n");
 		return (0);
-	}
 	while (cents > 0)
 	{
 		if (cents >= 20)
-		{
 			coins += cents / 20;
 			cents %= 20;
-		}
 		else if (cents >= 5)
 		{
 			coins += cents / 5;
@@ -49,6 +41,5 @@ int main(int argc, char *argv[])
 		}
 	}
 	printf("%d\n", coins);
-
 	return (0);
 }
