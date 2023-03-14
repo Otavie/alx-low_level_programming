2613e1d610018afb4195b2470cfa7501a91af9f5
Otavie <otavieokuoyo@gmail.com>
2023-03-14 12:39:09 -0700

    Added the 1-strdup.c file

--- 0x0B-malloc_free/11.c
+++ 0x0B-malloc_free/11.c
@@ -13,12 +13,9 @@ char *_strdup(char *str)
 {
 	char *dup_str;
 
-	if (str == NULL)
-		return (NULL);
-
 	dup_str = malloc(sizeof(char) * (strlen(str) + 1));
 
-	if (dup_str == NULL)
+	if ((str == NULL) || (dup_str == NULL))
 		return (NULL);
 
 	strcpy(dup_str, str);
