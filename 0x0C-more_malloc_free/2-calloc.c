ffe2832ebb591978a52b694a19a1799a1bc49c26
Otavie <otavieokuoyo@gmail.com>
2023-03-16 14:43:47 -0700

    Added the 2-calloc.c file

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stidlib.h>
+
+/**
+ * _calloc - Allocates memory for an array, using malloc
+ *
+ * @nmemb: The number of elements to allocate
+ * @size: The size of each element in bytes
+ *
+ * Return: A pointer to the allocated memory or NULL on failure
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+
+	if (p != NULL)
+	{
+		char *byte_p = p;
+
+		for (i = 0; i < nmemb * size; i++)
+			byte_p[i] = 0;
+
+	}
+
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+	unsigned int i;
+}
