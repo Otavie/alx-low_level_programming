a51c7bd4dd7b5b57e5a3fde5b114109c13cb685b
Otavie <otavieokuoyo@gmail.com>
2023-03-16 15:35:53 -0700

    Added the 3-array_range.c file

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -29,12 +29,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (old_size == new_size)
 		return (ptr);
 
-	new_ptr = maloc(new_size);
+	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
 
 	if (new_size > old_size)
-		memcpy(new_ptr, ptr, old_ptr);
+		memcpy(new_ptr, ptr, old_size);
 	else
 		memcpy(new_ptr, ptr, new_size);
 	free(ptr);
