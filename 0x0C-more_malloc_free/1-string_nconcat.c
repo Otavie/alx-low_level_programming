a6645b18c5904b4217078161f5743c2f0cb88933
Otavie <otavieokuoyo@gmail.com>
2023-03-16 14:31:24 -0700

    Modified the 1-string_nconcat.c file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -38,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < len1; i++)
 		concatenated_string[i] = s1[i];
 
-	for (j = 0; j < len2; j++)
+	for (j = 0; j < n; j++)
 		concatenated_string[i + j] = s2[j];
 
 	concatenated_string[i + j] = '\0';
