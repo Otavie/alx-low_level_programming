20e402e23b39b55aafa576e4758b4df789f4b420
Otavie <otavieokuoyo@gmail.com>
2023-03-22 15:09:43 -0700

    Modified the dog.h file to include betty style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - A new type describing a dog
+ *
+ * @name: The name of the dog
+ * @age: THe age of the dog
+ * @owner: The name of the owner of the dog
+ */
 struct dog
 {
 	char *name;
