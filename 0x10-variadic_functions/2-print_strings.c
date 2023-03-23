6993229f82a529998fd7c6b51859712d9019da33
Otavie <otavieokuoyo@gmail.com>
2023-03-23 08:51:26 -0700

    Added the 2-print_string.c file

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,8 +26,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (str == NULL)
 			printf("nil");
-		else
-			printf("%s", str);
 
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
