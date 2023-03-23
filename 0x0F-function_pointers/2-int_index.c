452a54e6e32495667af3c178aa0af3525d4b0fb7
Otavie <otavieokuoyo@gmail.com>
2023-03-22 19:13:43 -0700

    Added the 2-int_index.c file

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,9 +5,9 @@
  *
  * @array: the array to search
  * @size: the number of elements in the array
- * @cmp: pointer to the function to be used 
+ * @cmp: pointer to the function to be used to compare values
  *
- * Return: 
+ * Return: the index of the first element
  */
 
 
@@ -15,14 +15,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
 		{
-				return (1);
+			return (1);
 		}
 	}
 
