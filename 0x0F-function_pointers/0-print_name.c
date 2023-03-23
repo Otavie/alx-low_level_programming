05193b5a20a12bb88d69f6c38c03b6a243aea753
Otavie <otavieokuoyo@gmail.com>
2023-03-22 19:26:11 -0700

    Modified the 0-pirnt_name.c file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,11 +2,10 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Prints a name
+ * print_name - prints a name
  *
- * @name: The name to be printed
- * @f: Pointer to the function to be used to print the name
- *
+ * @name: the name to be printed
+ * @f: pointer to the function to be used to print the name
  */
 void print_name(char *name, void (*f)(char *))
 {
