c4b9cc456f5c556325308c12217d1833d02e842a
Otavie <otavieokuoyo@gmail.com>
2023-03-23 08:58:14 -0700

    Modified the 2-print_string.c file

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,9 +15,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list args_list;
 	const char *str;
 
-	if (n == 0)
-		return;
-
 	va_start(args_list, n);
 
 	for (i = 0; i < n; i++)
