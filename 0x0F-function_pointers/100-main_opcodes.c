ce08038ecfc38003ece524d10759880c80033944
Otavie <otavieokuoyo@gmail.com>
2023-03-22 19:48:49 -0700

    Added the file 100-main_opcodes.c file

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -16,3 +16,24 @@ void print_opcodes(char *start_address, int size)
 	}
 	printf("\n");
 }
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	print_opcodes((char *)&main, num_bytes);
+
+	return (0);
+}
