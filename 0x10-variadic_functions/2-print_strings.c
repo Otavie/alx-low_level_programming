8613223f7469f73e19758cc3feda8b5a0601de63
Otavie <otavieokuoyo@gmail.com>
2023-03-23 09:07:17 -0700

    Modified the 2-print_string.c file

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		str = va_arg(args_list, const char *);
 
 		if (str == NULL)
-			printf("nil");
+			printf("(nil)");
 		else
 			printf("%s", str);
 
