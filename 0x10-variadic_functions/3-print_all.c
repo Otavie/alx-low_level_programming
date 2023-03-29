8ff74ab5472685391ca0f9e3f3341eb415c11f10
Otavie <otavieokuoyo@gmail.com>
2023-03-29 16:30:19 -0700

    Modified previous code

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,51 +2,109 @@
 #include <stdio.h>
 
 /**
- * print_all - Prints anything
- * @format: A list of types of arguments passed to the function
- * s: char * (if the string is NULL, orint (nil)
+ * print_char - Prints a character argument
+ *
+ * @args: List of arguments
+ * @first_arg: Pointer to a flag indicating if this is the first argument
+ */
+void print_char(va_list args, int *first_arg)
+{
+	char c = (char)va_arg(args, int);
+
+	if (!(*first_arg))
+		printf(", ");
+	printf("%c", c);
+}
+
+/**
+ * print_int - Prints an integer arguments
+ *
+ * @args: List arguments
+ * @first_arg: Pointer to a flag indicating if this is the first argument
+ */
+void print_int(va_list args, int *first_arg)
+{
+	int i = va_arg(args, int);
+
+	if (!(*first_arg))
+		printf(", ");
+	printf("%d", i);
+}
+
+/**
+ * print_float - Prints a floating-point argument
+ *
+ * @args: List of arguments
+ * @first_arg: Pointer to a flag indicating if this is the first argument
+ */
+void print_float(va_list args, int *first_arg)
+{
+	float f = (float)va_arg(args, double);
+
+	if (!(*first_arg))
+		printf(", ");
+	printf("%f", f);
+}
+
+/**
+ * print_string - Prints a string argument
+ *
+ * @args: List of arguments
+ * @first_arg: Pointer to a flag indicating if this is the first argument
+ */
+void print_string(va_list args, int *first_arg)
+{
+	char *s = va_arg(args, char *);
+
+	if (!(*first_arg))
+		printf(", ");
+	if (s == NULL)
+	{
+		printf("(nil)");
+	}
+	else
+	{
+		printf("%s", s);
+	}
+}
+
+/**
+ * print_all - Prints a list of arguments based on a given format
+ *
+ * @format: A string of character representing the format of the argument
  *
  * Return: Void
  */
 void print_all(const char * const format, ...)
 {
-	va_list args_list;
+	va_list args;
+	int first_arg;
 	const char *p;
-	char *s;
 
-	va_start(args_list, format);
-	for (p = format; *p != '\0'; p++)
+	va_start(args, format);
+	first_arg = 1;
+	p = format;
+	while (*p)
 	{
 		switch (*p)
 		{
 			case 'c':
-				printf("%c", va_arg(args_list, int));
-				if (*(p + 1) != '\0' && (*(p + 1) == 'c' || *(p + 1) == 'i' || *(p + 1) == 'e' || *(p + 1) == 's'))
-					printf(", ");
+				print_char(args, &first_arg);
 				break;
 			case 'i':
-				printf("%d", va_arg(args_list, int));
-				if (*(p + 1) != '\0' && (*(p + 1) == 'c' || *(p + 1) == 'i' || *(p + 1) == 'e' || *(p + 1) == 's'))
-					printf(", ");
+				print_int(args, &first_arg);
 				break;
-			case 'e':
-				printf("%f", (float)va_arg(args_list, double));
-				if (*(p + 1) != '\0' && (*(p + 1) == 'c' || *(p + 1) == 'i' || *(p + 1) == 'e' || *(p + 1) == 's'))
-					printf(", ");
+			case 'f':
+				print_float(args, &first_arg);
 				break;
 			case 's':
-				s = va_arg(args_list, char *);
-				if (s == NULL)
-					printf("(nil)");
-				else
-					printf("%s", s);
-				if (*(p + 1) != '\0' && (*(p + 1) == 'c' || *(p + 1) == 'i' || *(p + 1) == 'e' || *(p + 1) == 's'))
-					printf(", ");
+				print_string(args, &first_arg);
 				break;
 			default:
 				break;
 		}
+		p++;
 	}
 	printf("\n");
-	va_end(args_list);
+	va_end(args);
 }
