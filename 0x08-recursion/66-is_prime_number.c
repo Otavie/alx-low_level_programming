6e89d981e28e8d356b2e21a1e3e0c8fe7c354706
Otavie <otavieokuoyo@gmail.com>
2023-03-29 14:14:47 -0700

    Modified the 6-is_prime_number.c file

--- 0x08-recursion/66-is_prime_number.c
+++ 0x08-recursion/66-is_prime_number.c
@@ -5,18 +5,26 @@
  *
  * @n: The number to be checked
  * @i: The current divisor to check
- * @j: The maximum possible divisor
  *
- * Return: 1 if prime or otherwise 0
+ * Return: 1 if prime or 0 otherwise 0
  */
-int is_prime_helper(int n, int i, int j)
+int is_prime_helper(int n, int i)
 {
-	if (i > j)
-		return (1);
-	else if (n % j == 0)
-		return (0);
+	int sqrt_n;
+
+	sqrt_n = 1;
+	while (sqrt_n * sqrt_n <= n)
+		sqrt_n++;
+
+	if (i <= sqrt_n)
+	{
+		if (n % i == 0)
+			return (0);
+		else
+			return (is_prime_helper(n, i + 1));
+	}
 	else
-		return (is_prime_helper(n, i, j - 1));
+		return (1);
 }
 
 
@@ -32,5 +40,5 @@ int is_prime_number(int n)
 	if (n <= 1)
 		return (0);
 	else
-		return (is_prime_helper(n, 2, n - 1));
+		return (is_prime_helper(n, 2));
 }
--- 0x08-recursion/77-is_prime_number.c
+++ 0x08-recursion/77-is_prime_number.c
@@ -1,24 +1,26 @@
 #include "main.h"
+#include <math.h>
 
 /**
  * is_prime_helper - a recursive helper function that checks for a prime number
  *
  * @n: The number to be checked
  * @i: The current divisor to check
- * @j: The maximum possible divisor
  *
  * Return: 1 if prime or otherwise 0
  */
-int is_prime_helper(int n, int i, int j)
+int is_prime_helper(int n, int i)
 {
-	if (i > j)
-		return (1);
-	else if (n % j == 0)
-		return (0);
+	if (i <= sqrt_n)
+	{
+		if (n % i == 0)
+			return (0);
+		else
+			return (is_prime_helper(n, i + 1, sqrt_n));
+	}
 	else
-		return (is_prime_helper(n, i, j - 1));
+		return (1);
 }
-
 
 /**
  * is_prime_number - Det. if an integer is prime by calling a recursive func
@@ -29,8 +31,12 @@ int is_prime_helper(int n, int i, int j)
  */
 int is_prime_number(int n)
 {
+	int sqrt_n;
+
 	if (n <= 1)
 		return (0);
-	else
-		return (is_prime_helper(n, 2, n - 1));
+
+	sqrt_n = sqrt(n);
+
+	return (is_prime_helper(n, 2, sqrt_n));
 }
