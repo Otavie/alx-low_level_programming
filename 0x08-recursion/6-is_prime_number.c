230d5b49c0bac009006175b715b34c1e3a581c91
Otavie <otavieokuoyo@gmail.com>
2023-03-29 13:33:21 -0700

    Modified the 6-is_prime_number.c file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,37 +2,47 @@
 #include <math.h>
 
 /**
- * is_prime_number - a function that checks for a prime number
+ * is_prime_helper - a recursive helper function that checks for a prime number
  *
- * @x: The number to be checked
+ * @n: The number to be checked
+ * @i: The current divisor to check
  *
- * Return: 1 if prime or 0 if not
+ * Return: 1 if prime or 0 otherwise 0
  */
-
 int is_prime_helper(int n, int i);
-int is_prime_number(int n);
 
-int is_prime_helper(int n, int i)
-{
-	if (n <= 1)
-	{
-		return (0);
-	}
-	else if (i > sqrt(n))
-	{
-		return (1);
-	}
-	else if (n % i == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		return (is_prime_helper(n, i + 1));
-	}
-}
-
+/**
+ * is_prime_number - Det. if an integer is prime by calling a recursive func
+ *
+ * @n: The integer to be checked
+ *
+ * Return: 1 if n is prime, otherwise 0
+ */
 int is_prime_number(int n)
 {
-	return (is_prime_helper(n, 2));
+	if (n <= 1)
+		return (0);
+	else
+		return (is_prime_helper(n, 2));
+}
+
+/**
+ * is_prime_helper - a recursive helper function that checks for a prime number
+ *
+ * @n: The number to be checked
+ * @i: The current divisor to check
+ *
+ * Return: 1 if prime or 0 otherwise 0
+ */
+int is_prime_helper(int n, int i)
+{
+	if (i <= sqrt(n))
+	{
+		if (n % i == 0)
+			return (0);
+		else
+			return (is_prime_helper(n, i + 1));
+	}
+	else
+		return (1);
 }
