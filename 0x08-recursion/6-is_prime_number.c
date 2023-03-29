bb593357f392d2a44d59b6923f7078b10460d227
Otavie <otavieokuoyo@gmail.com>
2023-03-29 13:48:05 -0700

    Modified the 6-is_prime_number.c file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <math.h>
 
 /**
  * is_prime_helper - a recursive helper function that checks for a prime number
@@ -9,7 +8,25 @@
  *
  * Return: 1 if prime or 0 otherwise 0
  */
-int is_prime_helper(int n, int i);
+int is_prime_helper(int n, int i)
+{
+	int sqrt_n;
+
+	sqrt_n = 1;
+	while (sqrt_n * sqrt_n <= n)
+		sqrt_n++;
+
+	if (i <= sqrt_n)
+	{
+		if (n % i == 0)
+			return (0);
+		else
+			return (is_prime_helper(n, i + 1));
+	}
+	else
+		return (1);
+}
+
 
 /**
  * is_prime_number - Det. if an integer is prime by calling a recursive func
@@ -24,25 +41,4 @@ int is_prime_number(int n)
 		return (0);
 	else
 		return (is_prime_helper(n, 2));
-}
-
-/**
- * is_prime_helper - a recursive helper function that checks for a prime number
- *
- * @n: The number to be checked
- * @i: The current divisor to check
- *
- * Return: 1 if prime or 0 otherwise 0
- */
-int is_prime_helper(int n, int i)
-{
-	if (i <= sqrt(n))
-	{
-		if (n % i == 0)
-			return (0);
-		else
-			return (is_prime_helper(n, i + 1));
-	}
-	else
-		return (1);
 }
