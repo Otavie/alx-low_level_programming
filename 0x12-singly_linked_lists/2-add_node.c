b47fff44d55ba52cdf4028ff5c791624c84e6e2b
Otavie <otavieokuoyo@gmail.com>
2023-03-30 08:01:58 -0700

    Modified the file 2-add_node.c file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "lists.h"
 #include <stdlib.h>
 #include <string.h>
@@ -13,17 +14,24 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+
+	if (head ==  NULL || str == NULL)
+		return (NULL);
+
 	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len = strlen(str);
 
+	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
-
-	return (new_node);
+	return (*head);
 }
