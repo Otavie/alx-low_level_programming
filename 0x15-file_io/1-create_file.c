9536c0b2dbfd8fbeb586c6a6217e378630b56a46
Otavie <otavieokuoyo.gmail.com>
2023-07-10 18:28:52 -0700

    Code modification the file 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,10 +27,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while (text_content[bytes_written] != '0')
+		while (text_content[bytes_written] != '\0')
 			bytes_written++;
 
-		if (write(file_des, text_content, bytes_written) == bytes_written)
+		if (write(file_des, text_content, bytes_written) != bytes_written)
 		{
 			close(file_des);
 			return (-1);
