5d259aac2429aa92014c01127748ebddd1ac1e5b
Otavie <otavieokuoyo@gmail.com>
2024-06-02 14:23:56 -0700

    Check if the position is empty

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,4 +29,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = current->next;
 		i++;
 	}
+
+	if (!current || (i != idx - 1))
+	{
+		free(new_node);
+		return (NULL);
+	}
 }
