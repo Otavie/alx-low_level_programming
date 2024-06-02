196402c242641a3aa3543fbff057c024ce838b57
Otavie <otavieokuoyo@gmail.com>
2024-06-02 12:27:56 -0700

    Modified the code for 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	while (*head != NULL)
+	while (head != NULL)
 	{
 		if (i == index)
 			return (head);
