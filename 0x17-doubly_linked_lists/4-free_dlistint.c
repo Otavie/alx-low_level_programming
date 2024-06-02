316ac412899fe30c2311d3259b7fb782a8ff0a97
Otavie <otavieokuoyo@gmail.com>
2024-06-02 08:05:50 -0700

    Add the code for 4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a dlistint_t list
+ * @head: Pointer to the head of the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
