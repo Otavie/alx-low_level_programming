3fd0eddb822ef23e9024b8d72bc296d3385a65ae
Otavie <otavieokuoyo@gmail.com>
2024-06-02 06:55:08 -0700

    Modified the code for 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -23,5 +23,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->prev = NULL;
 	new_node->next = *head;
 
+	if (*head != NULL)
+		(*head)->prev = new_node;
+
+	*head = new_node;
+
 	return (new_node);
 }
