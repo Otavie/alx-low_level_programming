2d12f8ec041eb3a4d3a46ea5f4339428a3456b96
Otavie <otavieokuoyo@gmail.com>
2024-06-02 14:44:16 -0700

    Add more details

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,4 +35,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		free(new_node);
 		return (NULL);
 	}
+
+	/* Insert new node at a given position */
+	new_node->next = current->next;
+	new_node->prev = current;
+	if (current->next)
+		current->next->prev = new_node;
+	curren->next = new_node;
+
+	return (new_node);
 }
