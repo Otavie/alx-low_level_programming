985c16222451b82b5c74cf54acd68b3009820a5e
Otavie <otavieokuoyo@gmail.com>
2024-06-02 07:59:26 -0700

    At the lastNode, temp.next points newNode n newNode.prev points to temp

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -33,5 +33,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	while (temp->next != NULL)
 		temp = temp->next;
 
+	temp->next = new_node;
+	new_node->prev = temp;
+
 	return (new_node);
 }
