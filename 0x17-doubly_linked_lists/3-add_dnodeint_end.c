97c3581ed3a47e04206c3c7a1367b9a1f98b3b2a
Otavie <otavieokuoyo@gmail.com>
2024-06-02 07:43:37 -0700

    Set head to new_node if head == NULL

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -23,5 +23,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
 	return (new_node);
 }
