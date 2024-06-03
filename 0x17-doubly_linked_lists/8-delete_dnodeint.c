6adb48378d518f24e915af8096a355c0471c5702
Otavie <otavieokuoyo@gmail.com>
2024-06-02 19:24:37 -0700

    Removed white space on ln 10 for 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,7 +7,7 @@
  * delete_dnodeint_at_index - Deletes the node at index of a dlistint_t list
  * @head: Double pointer to the head of the list
  * @index: Index of the node that should be deleted
- * 
+ *
  * Return: 1 if it succeed, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
