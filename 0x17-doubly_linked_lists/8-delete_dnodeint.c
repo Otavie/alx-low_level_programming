08e3e644467ecade9b617641ed0933b547a15084
Otavie <otavieokuoyo@gmail.com>
2024-06-02 19:29:27 -0700

    Modified the codes in 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -6,24 +6,49 @@
 /**
  * delete_dnodeint_at_index - Deletes the node at index of a dlistint_t list
  * @head: Double pointer to the head of the list
- * @index: Index of the node that should be deleted
+ * @index: Index of the node that should be deleted. Index starts at 0
  *
- * Return: 1 if it succeed, -1 if it failed
+ * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *current = *head;
 	unsigned int i = 0;
 
-	while (head != NULL)
-	{
-		if (i == index)
-		{
-			head = head->next;
-			free(head);
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
 
-			return (1);
-		}
+	new_node->n = n;
+
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		new_node->prev = NULL;
+		if (*h)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
 	}
 
-	return (0);
+	while (current && i < idx - 1)
+	{
+		current = current->next;
+		i++;
+	}
+
+	if (!current || (i != idx - 1))
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	/* Insert new node at a given position */
+	new_node->next = current->next;
+	new_node->prev = current;
+	if (current->next)
+		current->next->prev = new_node;
+	current->next = new_node;
+
+	return (new_node);
 }
