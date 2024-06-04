2d0b8b09fe0cbf3732f73db0068e47fa194bbd3f
Otavie <otavieokuoyo@gmail.com>
2024-06-03 18:17:26 -0700

    All declaration in one line 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,8 +10,7 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash_value;
-	unsigned long int index;
+	unsigned long int hash_value, index;
 
 	hash_value = hash_djb2(key);
 	index = hash_value % size;
