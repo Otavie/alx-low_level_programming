1011fd65ab9fab104df65bccac07b4b011c202b5
Otavie <otavieokuoyo@gmail.com>
2024-06-03 17:38:57 -0700

    Add code descriptions for 1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,11 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Hash function implementing the djb2 algorithm
+ * @str: The string to hash
+ *
+ * Return: The hash value
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
